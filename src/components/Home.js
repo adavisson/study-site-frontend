5ef55e1d0651326c59c55c1b3346e1ace8b1b6fc
Andrew Davisson <akdavisson4@gmail.com>
2020-03-21 12:27:39 -0600

    created home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from 'semantic-ui-react';
+
+const Home = () => {
+  return (
+    <div>
+      <Header as="h1">Study Site</Header>
+      <div className="card-container">
+        // add cards here
+      </div>
+    </div>
+  );
+}
+ 
+export default Home;
